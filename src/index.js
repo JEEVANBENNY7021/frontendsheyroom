84d331dc0d3b0ef071dfcb07043d36f15357a565
JEEVAN BENNY <jeevanbenny2002@gmail.com>
2025-01-25 18:01:15 +0530

    Add Vercey

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'; // Correct import for React 18+
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
